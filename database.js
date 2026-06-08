a64753dacc3a5c463e53d96942957e4c98ebab81
josmanver7-lang <josmanver7@gmail.com>
2026-06-07 21:23:59 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,5 @@
 import { MongoClient } from "mongodb";
 
-import { MongoClient } from "mongodb";
-
 console.log("MONGODB_URI =", process.env.MONGODB_URI);
 
 const client = new MongoClient(process.env.MONGODB_URI);
