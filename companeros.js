35bfa56c9d02fd02fe7ccd5454c40fd589adc4cf
josmanver7-lang <josmanver7@gmail.com>
2026-06-09 00:21:57 -0400

    Create companeros.js

---
+++ companeros.js
@@ -0,0 +1,57 @@
+export const companions = {
+  alteru: {
+    nombre: "Altéru",
+    coste: 500,
+    clase: "Capitán",
+    afinidadMax: 100,
+    habilidad: "Capitán de las Colinas"
+  },
+
+  nieriel: {
+    nombre: "Nieriel",
+    coste: 150,
+    clase: "Capitán",
+    afinidadMax: 100,
+    habilidad: "Sangre Élfica"
+  },
+
+  faelon: {
+    nombre: "Faelon",
+    coste: 100,
+    clase: "Guardián Rúnico",
+    afinidadMax: 100,
+    habilidad: "Sabiduría de Rivendel"
+  },
+
+  cirdil: {
+    nombre: "Círdil",
+    coste: 250,
+    clase: "Guerrero",
+    afinidadMax: 100,
+    habilidad: "Escudo de Gondor"
+  },
+
+  andaer: {
+    nombre: "Andaer",
+    coste: 150,
+    clase: "Guerrero",
+    afinidadMax: 100,
+    habilidad: "Escudero Leal"
+  },
+
+  duilon: {
+    nombre: "Duilon",
+    coste: 200,
+    clase: "Campeón",
+    afinidadMax: 100,
+    habilidad: "Campeón Indomable"
+  },
+
+  montaraces: {
+    nombre: "Montaraces de Arathir",
+    coste: 1000,
+    clase: "Cazadores",
+    afinidadMax: 100,
+    habilidad: "Exploradores del Norte"
+  }
+};
