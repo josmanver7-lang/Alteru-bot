1c5bd81955d60466de23f266d060b6a5377d68d2
josmanver7-lang <josmanver7@gmail.com>
2026-06-14 22:30:52 -0400

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,4 +1,4 @@
-⅞import { readFile } from "node:fs/promises";
+import { readFile } from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import * as db from "./database.js";
