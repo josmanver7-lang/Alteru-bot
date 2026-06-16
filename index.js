5b1a58bfa0347d3a56aa7332e2574771cb045fdd
josmanver7-lang <josmanver7@gmail.com>
2026-06-16 08:48:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -752,7 +752,6 @@ function getCompanionBasePower(companionId) {
   const key = normalizeKey(companionId);
 
   const personajeLoadout = getCompanionEquipmentFromPersonaje(key);
-  const fallbackLoadout = COMPANION_BASE_EQUIPMENT[key] || {};
 
   const loadout = Object.keys(personajeLoadout).length
     ? personajeLoadout
