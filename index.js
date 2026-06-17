c1c154af87d21185355689ce9c7103e728006b0d
josmanver7-lang <josmanver7@gmail.com>
2026-06-17 14:04:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ function getExplorationClosingText(points, item, rarity) {
   }
 
   if (!hasItem && points < 150) {
-    return "No estuvo mal, para la próxima llevaré mas hierba de pipa para calmar las ansias.";
+    return "No estuvo mal, para la próxima llevaré mas hoja de la cuaderna para calmar las ansias.";
   }
 
   if (!hasItem) {
