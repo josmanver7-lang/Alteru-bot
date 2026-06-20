6cd2612e572dc43d53719459546664c9ae64aa2c
josmanver7-lang <josmanver7@gmail.com>
2026-06-20 18:51:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1056,7 +1056,7 @@ function getCompanionEquipmentFromPersonaje(companionId) {
   } else {
     const armaEquipada = rawEq.arma || personaje.arma || "";
     const esDosManos = armaEquipada && (armaEquipada.hands === 2 || armaEquipada.slot === "arma_2_manos");
-    const logarmaequipada 
+    consol.logarmaequipada 
     
     Object.assign(formattedEq, {
       arma: armaEquipada,
